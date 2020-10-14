c0c05fe7eb416d9fcbba1228d74ad0e3c4c2b0e9
ardasoyturk <33841775+ardasoyturk@users.noreply.github.com>
2020-10-14 12:24:42 +0300

    URL support to multiple downloads

--- src/names.js
+++ src/names.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const ytdl = require('ytdl-core');
-
+const videoRegex = /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?$/g;
 /**
  * @param {String} tracks An array of song names.
  * @param {String} dir Output folder for downloaded songs.
@@ -10,12 +10,14 @@ const ytdl = require('ytdl-core');
  async function downloadSongsByNames(tracks, dir) {
     const ytsr = require('ytsr');
     for (song of tracks) {
-        let track = (await ytsr(song, { limit: 1 }))?.items[0]?.link;
+        let track;
+        if (videoRegex.test(song)) track = song;
+        else track = (await ytsr(song, { limit: 1 }))?.items[0]?.link;
         if (!track) {
             console.error('An error occured, please try again.')
             continue;
         }
-        ytdl(track, { filter: 'audioonly' }).pipe(fs.createWriteStream(path.join(dir, song + '.mp3')));
+        ytdl(track, { filter: 'audioonly' }).pipe(fs.createWriteStream(path.join(dir, song.replace(videoRegex, `Track ${fs.readdirSync(dir).filter(x => x.endsWith('.mp3')).length+1}`) + '.mp3')));
     }
 };
 
